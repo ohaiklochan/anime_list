72cda6648bb80b61a2aeb1048ad8135478462968
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-07-08 08:27:39 -0500

    controller edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,38 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
+  get '/' do
+      erb :index
+  end
+
+  get '/home' do
+      authenticate
+      erb :home
+  end
+
+  helpers do
+    
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
+    def authenticate
+        if !logged_in?
+            redirect '/login'
+        else
+            @user = current_user
+            @user_anime_list = current_user.anime_list
+        end
+    end
+
+    def authenticate_user(anime_list)
+        redirect '/home' if !anime_list
+        redirect '/home' if current_user != anime_list.user
+    end
+    
+  end
 end
