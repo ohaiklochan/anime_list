ff27c7eebcd0f8591ce3136025439bbbecb0655a
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-07-09 20:56:53 -0500

    modified model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :animelist
-    has_many :anime, through: :animelist
+    validates :email, :username, :password, presence: true
 end
