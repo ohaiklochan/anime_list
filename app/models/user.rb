0110a9ce3d33b1b5dd7dd791660e2ef1d7e63d23
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-07-09 23:51:09 -0500

    fixed error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :animelist
-    validates :email, :username, :password, presence: true
+    validates :email, :username, :password_digest, presence: true
 end
